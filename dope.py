f2ca24707046a68ffcd2ecddfba8590575c98350
Bruno <vollmer.bruno@googlemail.com>
2020-08-23 12:25:41 +0200

    rewrote dope estimator

--- dope.py
+++ dope.py
@@ -1,212 +1,105 @@
-# Copyright 2020-present NAVER Corp.
-# CC BY-NC-SA 4.0
-# Available only for non-commercial use
-
-import sys, os
-import argparse
-import os.path as osp
-from PIL import Image
-import cv2
+import os
+import sys
 import numpy as np
-
+import cv2
 import torch
 from torchvision.transforms import ToTensor
+from PIL import Image
 
-_thisdir = osp.realpath(osp.dirname(__file__))
+# add ppi repo to sys path and import it
+curr_dir = os.path.realpath(os.path.dirname(__file__))
+sys.path.append(os.path.join(curr_dir, 'lcrnet-v2-improved-ppi'))
 
-sys.path.append( _thisdir+'/lcrnet-v2-improved-ppi/')
 try:
     from lcr_net_ppi_improved import LCRNet_PPI_improved
 except ModuleNotFoundError:
     raise Exception('To use the pose proposals integration (ppi) as postprocessing, please follow the readme instruction by cloning our modified version of LCRNet_v2.0 here. Alternatively, you can use --postprocess nms without any installation, with a slight decrease of performance.')
 
-
 from model import dope_resnet50, num_joints
-import postprocess
-
-import visu
-
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-
-def load_model(modelname, no_half=False):
-
-    # load model
-    ckpt_fname = osp.join(_thisdir, 'models', modelname+'.pth.tgz')
-    if not os.path.isfile(ckpt_fname):
-        raise Exception('{:s} does not exist, please download the model first and place it in the models/ folder'.format(ckpt_fname))
-    print('Loading model', modelname)
-    ckpt = torch.load(ckpt_fname)
-    if no_half:
-        ckpt['half'] = False # uncomment this line in case your device cannot handle half computation
-    ckpt['dope_kwargs']['rpn_post_nms_top_n_test'] = 1000
-    model = dope_resnet50(**ckpt['dope_kwargs'])
-    if ckpt['half']:
-        model = model.half()
-    model = model.eval()
-    model.load_state_dict(ckpt['state_dict'])
-    model = model.to(device)
-
-    return model, ckpt
-
-def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
-
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation = inter)
-
-    # return the resized image
-    return resized
-
-def dope_image(imagename, model, ckpt, postprocessing='ppi'):
+from postprocess import assign_hands_and_head_to_body
+from visu import visualize_bodyhandface2d
 
 
-    # load the image
-    print('Loading image', imagename)
-    image = Image.open(imagename)
-    imlist = [ToTensor()(image).to(device)]
-    if ckpt['half']:
-        imlist = [im.half() for im in imlist]
-    resolution = imlist[0].size()[-2:]
+class DopeEstimator:
 
-    # forward pass of the dope network
-    print('Running DOPE')
-    with torch.no_grad():
-        results = model(imlist, None)[0]
+    def __init__(self, model_path='models/DOPErealtime_v1_0_0.pth.tgz', use_half_comp=True):
+        self._load_model(model_path, use_half_comp)
 
-    # postprocess results (pose proposals integration, wrists/head assignment)
-    print('Postprocessing')
-    assert postprocessing in ['nms','ppi']
-    parts = ['body','hand','face']
-    if postprocessing=='ppi':
-        res = {k: v.float().data.cpu().numpy() for k,v in results.items()}
+
+    def _load_model(self, model_path, use_half_comp):
+        self._device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+        if not os.path.isfile(model_path):
+            raise Exception(
+                '{:s} does not exist. Please use correct model path.'.format(model_path))
+
+        ckpt = torch.load(model_path)
+
+        if not use_half_comp:
+            ckpt['half'] = False
+
+        ckpt['dope_kwargs']['rpn_post_nms_top_n_test'] = 1000
+
+        model = dope_resnet50(**ckpt['dope_kwargs'])
+
+        if ckpt['half']:
+            model = model.half()
+
+        model = model.eval()
+        model.load_state_dict(ckpt['state_dict'])
+        model = model.to(self._device)
+
+        self._model = model
+        self._ckpt = ckpt
+
+    def _post_process(self, results):
+
+        parts = ['body', 'hand', 'face']
+
+        res = {k: v.float().data.cpu().numpy() for k, v in results.items()}
+
+
         detections = {}
         for part in parts:
-            detections[part] = LCRNet_PPI_improved(res[part+'_scores'], res['boxes'], res[part+'_pose2d'], res[part+'_pose3d'], resolution, **ckpt[part+'_ppi_kwargs'])
-    else: # nms
-        detections = {}
-        for part in parts:
-            dets, indices, bestcls = postprocess.DOPE_NMS(results[part+'_scores'], results['boxes'], results[part+'_pose2d'], results[part+'_pose3d'], min_score=0.3)
-            dets = {k: v.float().data.cpu().numpy() for k,v in dets.items()}
-            detections[part] = [{'score': dets['score'][i], 'pose2d': dets['pose2d'][i,...], 'pose3d': dets['pose3d'][i,...]} for i in range(dets['score'].size)]
-            if part=='hand':
-                for i in range(len(detections[part])):
-                    detections[part][i]['hand_isright'] = bestcls<ckpt['hand_ppi_kwargs']['K']
-    # assignment of hands and head to body
-    detections = postprocess.assign_hands_and_head_to_body(detections)
+            detections[part] = LCRNet_PPI_improved(
+                res[part+'_scores'], res['boxes'], res[part+'_pose2d'], res[part+'_pose3d'], self._resolution, **self._ckpt[part+'_ppi_kwargs'])
 
-    # display results
-    print('Displaying')
-    det_poses2d = {part: np.stack([d['pose2d'] for d in part_detections], axis=0) if len(part_detections)>0 else np.empty( (0,num_joints[part],2), dtype=np.float32) for part, part_detections in detections.items()}
-    scores = {part: [d['score'] for d in part_detections] for part,part_detections in detections.items()}
-    imout = visu.visualize_bodyhandface2d(np.asarray(image)[:,:,::-1],
-                                          det_poses2d,
-                                          dict_scores=scores,
-                                         )
+        # assignment of hands and head to body
+        detections = assign_hands_and_head_to_body(detections)
 
-    cv2.imshow("result", imout)
+        return detections
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    def _visualize_results(self, results, image):
+        det_poses2d = {part: np.stack([d['pose2d'] for d in part_detections], axis=0) if len(part_detections) > 0 else np.empty(
+            (0, num_joints[part], 2), dtype=np.float32) for part, part_detections in results.items()}
 
-def dope_video(video_name, model, ckpt, postprocessing='ppi'):
+        scores = {part: [d['score'] for d in part_detections]
+                  for part, part_detections in results.items()}
 
-    cap = cv2.VideoCapture(video_name)
+        res_img = visualize_bodyhandface2d(np.asarray(
+            image)[:, :, ::-1], det_poses2d, dict_scores=scores)
 
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        print("next frame")
+        cv2.imshow("result", res_img)
 
-        if ret:
-            frame = image_resize(frame, width=300)
+        cv2.waitKey(0)
 
-            image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    def run(self, image, visualize=False):
+        # convert to PIL image
+        image = Image.fromarray(image)
 
-            imlist = [ToTensor()(image).to(device)]
-            if ckpt['half']:
-                imlist = [im.half() for im in imlist]
-            resolution = imlist[0].size()[-2:]
+        tensor_list = [ToTensor()(image).to(self._device)]
 
-            # forward pass of the dope network
+        if self._ckpt['half']:
+            tensor_list = [t.half() for t in tensor_list]
 
-            with torch.no_grad():
-                results = model(imlist, None)[0]
+        self._resolution = tensor_list[0].size()[-2:]
 
-            # postprocess results (pose proposals integration, wrists/head assignment)
-            assert postprocessing in ['nms','ppi']
-            parts = ['body','hand','face']
-            if postprocessing=='ppi':
-                res = {k: v.float().data.cpu().numpy() for k,v in results.items()}
-                detections = {}
-                for part in parts:
-                    detections[part] = LCRNet_PPI_improved(res[part+'_scores'], res['boxes'], res[part+'_pose2d'], res[part+'_pose3d'], resolution, **ckpt[part+'_ppi_kwargs'])
-            else: # nms
-                detections = {}
-                for part in parts:
-                    dets, indices, bestcls = postprocess.DOPE_NMS(results[part+'_scores'], results['boxes'], results[part+'_pose2d'], results[part+'_pose3d'], min_score=0.3)
-                    dets = {k: v.float().data.cpu().numpy() for k,v in dets.items()}
-                    detections[part] = [{'score': dets['score'][i], 'pose2d': dets['pose2d'][i,...], 'pose3d': dets['pose3d'][i,...]} for i in range(dets['score'].size)]
-                    if part=='hand':
-                        for i in range(len(detections[part])):
-                            detections[part][i]['hand_isright'] = bestcls<ckpt['hand_ppi_kwargs']['K']
-            # assignment of hands and head to body
-            detections = postprocess.assign_hands_and_head_to_body(detections)
+        with torch.no_grad():
+            results = self._model(tensor_list, None)[0]
 
-            # display results
-            det_poses2d = {part: np.stack([d['pose2d'] for d in part_detections], axis=0) if len(part_detections)>0 else np.empty( (0,num_joints[part],2), dtype=np.float32) for part, part_detections in detections.items()}
-            scores = {part: [d['score'] for d in part_detections] for part,part_detections in detections.items()}
-            imout = visu.visualize_bodyhandface2d(np.asarray(image)[:,:,::-1],
-                                                det_poses2d,
-                                                dict_scores=scores,
-                                                )
+        post_proc_results = self._post_process(results)
 
-            cv2.imshow("result", imout)
-            cv2.waitKey(1)
+        if visualize:
+            self._visualize_results(post_proc_results, image)
 
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(description='running DOPE on an image: python dope.py --model <modelname> --image <imagename>')
-
-    parser.add_argument('--model', required=True, type=str, help='name of the model to use (eg DOPE_v1_0_0)')
-
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--image', type=str, help='path to the image')
-    group.add_argument('--video', type=str, help='path to video')
-
-    parser.add_argument('--postprocess', default='ppi', choices=['ppi','nms'], help='postprocessing method')
-    parser.add_argument('--no_half', action='store_true', help='disable half computation')
-
-    args = parser.parse_args()
-
-    model, ckpt = load_model(args.model, no_half=args.no_half)
-
-    if args.image:
-        dope_image(args.image, model, ckpt, postprocessing=args.postprocess)
-    else:
-        dope_video(args.video, model, ckpt, postprocessing=args.postprocess)
+        return post_proc_results
---
+++ main.py
@@ -0,0 +1,49 @@
+import sys, os
+import argparse
+import cv2
+
+from dope import DopeEstimator
+from util import resize_image
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='running DOPE on an image: python dope.py --model <modelname> --image <imagename>')
+
+    parser.add_argument('--model', required=True, type=str, help='name of the model to use (eg DOPE_v1_0_0)')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--image', type=str, help='path to the image')
+    group.add_argument('--video', type=str, help='path to video')
+
+    parser.add_argument('--no_half_comp', action='store_true', help='disable half computation')
+
+    args = parser.parse_args()
+
+    dope = DopeEstimator(args.model, not args.no_half_comp)
+
+    if args.image:
+        image = cv2.imread(args.image)
+        image = resize_image(image, width=500)
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        results = dope.run(image, visualize=True)
+
+        cv2.destroyAllWindows()
+
+    else:
+        cap = cv2.VideoCapture(args.video)
+
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            frame = resize_image(frame, width=500)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            results = dope.run(frame, visualize=True)
+
+
+        cap.release()
+        cv2.destroyAllWindows()
